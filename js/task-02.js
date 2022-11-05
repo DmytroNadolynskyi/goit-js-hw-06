245f174c06294312c7594be955328799096425b0
DmytroNadolynskyi <113470234+DmytroNadolynskyi@users.noreply.github.com>
2022-11-05 11:13:31 +0200

    hw-js-06up2

--- js/task-02.js
+++ js/task-02.js
@@ -6,14 +6,23 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
-const list = document.querySelector('#ingredients')
-const ingArr = []
+// const list = document.querySelector('#ingredients')
+// const ingArr = []
 
-ingredients.forEach(ingredient => {
-	const item = document.createElement('li')
-	item.className = 'item'
-	item.textContent = ingredient
-	ingArr.push(item)
-})
+// ingredients.forEach(ingredient => {
+// 	const item = document.createElement('li')
+// 	item.className = 'item'
+// 	item.textContent = ingredient
+// 	ingArr.push(item)
+// })
 
-list.append(...ingArr)
+// list.append(...ingArr)
+
+
+const listEl = document.querySelector('#ingredients');
+ingredients.map(elem => {
+  const itemEl = document.createElement('li');
+  itemEl.classList.add('item');
+  itemEl.textContent = elem;
+  listEl.append(itemEl);
+});
